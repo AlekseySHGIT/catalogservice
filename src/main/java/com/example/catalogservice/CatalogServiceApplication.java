1e17ee3185402507ef7bf19c202938c1987b8424
ashitikov <ashitikov@ec-leasing.ru>
2021-01-12 16:39:57 +0300

    release444555

--- src/main/java/com/example/catalogservice/CatalogServiceApplication.java
+++ src/main/java/com/example/catalogservice/CatalogServiceApplication.java
@@ -9,5 +9,5 @@ public class CatalogServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(CatalogServiceApplication.class, args);
     }
-//2342344444433323423434534534
+//2342344444433564323423434534534
 }
